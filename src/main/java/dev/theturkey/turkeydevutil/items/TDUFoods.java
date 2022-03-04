a0a0c932e618322f665d1af2e7cb7edd5f041d2b
TheTurkeyDev <turkey@theturkey.dev>
2022-03-03 20:04:10 -0500

    Added Toast item Added Book item Added crafting recipe for lime seeds

--- src/main/java/dev/theturkey/turkeydevutil/items/TDUFoods.java
+++ src/main/java/dev/theturkey/turkeydevutil/items/TDUFoods.java
@@ -8,6 +8,7 @@ public class TDUFoods
 	public static final FoodProperties RAMEN = getFoodProps(5, 0.6f);
 	public static final FoodProperties ROCK_SOUP = getFoodProps(4, 0.25f);
 	public static final FoodProperties ICE_CREAM = getFoodProps(2, 0.9f);
+	public static final FoodProperties TOAST = getFoodProps(7, 0.7F);
 
 	public static FoodProperties getFoodProps(int nutrition, float saturation)
 	{
--- src/main/java/dev/theturkey/turkeydevutil/items/TDUItems.java
+++ src/main/java/dev/theturkey/turkeydevutil/items/TDUItems.java
@@ -14,11 +14,14 @@ import net.minecraftforge.fml.common.Mod;
 public class TDUItems
 {
 	public static TDUBaseItem LANYARD;
+	public static TDUBaseItem BOOK;
 
 	public static TDUBaseItem LIME;
 	public static TDUBaseItem ROCK_SOUP;
 	public static TDUBaseItem RAMEN;
 	public static TDUBaseItem ICE_CREAM;
+	public static TDUBaseItem TOAST;
+
 	public static TDUBaseBlockItem LIME_SEEDS;
 
 	public static TDUArmor COBBLESTONE_HELMET;
@@ -30,6 +33,9 @@ public class TDUItems
 	public static void onItemRegistry(RegistryEvent.Register<Item> e)
 	{
 		e.getRegistry().register(LANYARD = new TDUBaseItem(initProps(), "lanyard"));
+		LANYARD.addLore("Coming Soon™");
+		e.getRegistry().register(BOOK = new TDUBaseItem(initProps(), "book"));
+		BOOK.addLore("How do I use this?");
 
 		e.getRegistry().register(LIME = new TDUBaseItem(initFoodProps(TDUFoods.LIME), "lime"));
 		LIME.addLore("What? Is that not how it works?");
@@ -38,6 +44,8 @@ public class TDUItems
 		e.getRegistry().register(ROCK_SOUP = new ItemRockSoup());
 		e.getRegistry().register(RAMEN = new TDUBaseItem(initFoodProps(TDUFoods.RAMEN), "ramen"));
 		e.getRegistry().register(LIME_SEEDS = new TDUBaseBlockItem(TDUBlocks.LIME_CROP, initProps(), "lime_seeds"));
+		e.getRegistry().register(TOAST = new TDUBaseItem(initFoodProps(TDUFoods.TOAST), "toast"));
+		//TOAST.addLore("DarkToasto");
 
 		e.getRegistry().register(COBBLESTONE_HELMET = new TDUArmor(TDUArmorMaterials.COBBLESTONE, EquipmentSlot.HEAD, initArmorProps()));
 		e.getRegistry().register(COBBLESTONE_CHESTPLATE = new TDUArmor(TDUArmorMaterials.COBBLESTONE, EquipmentSlot.CHEST, initArmorProps()));
